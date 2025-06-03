edc41aa8c62d902de7be6a05f8000b12aadd171b
Coco <cocochen12301@gmail.com>
2025-06-03 02:24:02 -0700

    feat: Add user to a chatroom

--- server/src/controllers/chatroom.controller.ts
+++ server/src/controllers/chatroom.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import Chatroom from '../models/chatroom.model';
+import User from '../models/user.model';
 
 export const createChatroom = async (req: any, res: Response): Promise<void> => {
   try {
@@ -66,3 +67,44 @@ export const getUserChatrooms = async (req: any, res: Response): Promise<void> =
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const addUserToChatroom = async (req: any, res: Response): Promise<void> => {
+  try {
+    const requesterId = req.user._id;
+    const chatroomId = req.params.id;
+    const { userIdToAdd } = req.body;
+
+    const chatroom = await Chatroom.findById(chatroomId);
+    // Check if chatroom exists
+    if (!chatroom) {
+      res.status(404).json({ message: "Chatroom not found" });
+      return;
+    }
+
+    // Check if the requesting user is a member of chatroom
+    if (!chatroom.members.includes(requesterId.toString())) {
+      res.status(403).json({ message: "You are not a member of this chatroom" });
+      return;
+    }
+
+    // Check if user to add is valid
+    const userToAdd = await User.findById(userIdToAdd);
+    if (!userToAdd) {
+      res.status(404).json({ message: "User to add not found" });
+      return;
+    }
+
+    // Check if the user to add is already a member
+    if (chatroom.members.includes(userIdToAdd)) {
+      res.status(400).json({ message: "User is already a member of the chatroom" });
+      return;
+    }
+
+    chatroom.members.push(userIdToAdd);
+    await chatroom.save();
+
+    res.status(200).json({ message: "User added to chatroom", chatroom });
+  } catch (error) {
+    
+  }
+}
--- server/src/routes/chatroom.route.ts
+++ server/src/routes/chatroom.route.ts
@@ -1,11 +1,19 @@
 import express from "express";
-import { createChatroom, getChatroom, getUserChatrooms } from "../controllers/chatroom.controller";
+import { addUserToChatroom, createChatroom, getChatroom, getUserChatrooms } from "../controllers/chatroom.controller";
 import { protectRoute } from "../middleware/auth.middleware";
 
 const router = express.Router();
 
+// Get all chatrooms user is a member of
 router.get("/mychatrooms", protectRoute, getUserChatrooms);
+
+// Add a user to a chatroom
+router.post("/:id/adduser", protectRoute, addUserToChatroom);
+
+// Get the chatroom from chatroomId
 router.get("/:id", protectRoute, getChatroom);
+
+// Create a new chatroom
 router.post("/", protectRoute, createChatroom);
 
 export default router;
