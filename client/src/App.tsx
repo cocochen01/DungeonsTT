d2cb8410da379851fc5f65a523b18538462207be
Coco <cocochen12301@gmail.com>
2025-07-01 02:50:03 -0700

    feat: CORS configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,21 +5,31 @@ import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
 import SignUpPage from "./pages/SignUpPage";
 import { Routes, Route } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 const App = () => {
+  const {authUser, checkAuth} = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
   return (
     <div>
 
-    <Navbar />
+      <Navbar />
 
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
-      <Route path="/signup" element={<SignUpPage/>} />
-      <Route path="/login" element={<LoginPage/>} />
-      <Route path="/settings" element={<SettingsPage/>} />
-      <Route path="/profile" element={<ProfilePage/>} />
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
+        <Route path="/profile" element={<ProfilePage/>} />
 
-    </Routes>
+      </Routes>
     </div>
   )
 }
