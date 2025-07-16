003dca8ccd819c7aa810ff40fe4fb64f568e3136
Coco <cocochen12301@gmail.com>
2025-07-15 23:28:30 -0700

    feat: Homepage UI

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,39 @@
+import LeftSidebar from "../components/LeftSidebar";
+import NoRoomSelected from "../components/NoRoomSelected";
+import RightSidebar from "../components/RightSidebar";
+import Tabletop from "../components/Tabletop";
+import { useChatStore } from "../store/useChatStore";
 
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const { selectedChatroom } = useChatStore();
 
-export default HomePage
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-[1600px] h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            
+            {/* LEFT: Rooms list */}
+            <div className="w-1/5 border-r border-base-300">
+              <LeftSidebar />
+            </div>
+
+            {/* CENTER: Tabletop */}
+            <div className="w-3/5 bg-base-200">
+              {selectedChatroom ? <Tabletop /> : <NoRoomSelected />}
+            </div>  
+
+            {/* RIGHT: other stuff */}
+            <div className="w-1/5 border-l border-base-300">
+              <RightSidebar />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
