7c36644eaf6894fab53c13b5735b8413225db6bc
Coco <cocochen12301@gmail.com>
2025-07-16 01:03:07 -0700

    feat: Get chatrooms in left sidebar

--- client/src/components/LeftSidebar.tsx
+++ client/src/components/LeftSidebar.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+
 const LeftSidebar = () => {
+  const { getChatrooms, chatrooms, selectedChatroom, setSelectedChatroom, isChatroomsLoading } = useChatStore();
+
+  const activeChatrooms = [];
+
+  useEffect(() => {
+    getChatrooms();
+  }, [getChatrooms]);
+  
   return (
     <div>LeftSidebar</div>
   )
