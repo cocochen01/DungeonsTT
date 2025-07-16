26d9309cfec78140f1aa2362bd28effcc60dc146
Coco <cocochen12301@gmail.com>
2025-07-15 17:37:07 -0700

    feat: Logging out sends you to log in page

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore"
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
+  const nav = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    nav("/login");
+  };
   return (
     <header
       className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg">
@@ -34,7 +40,7 @@ const Navbar = () => {
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 items-center" onClick={logout}>
+                <button className="flex gap-2 items-center" onClick={handleLogout}>
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
