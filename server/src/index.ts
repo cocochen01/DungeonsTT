559517bb5c04ee7768bdb80b35433112240da01e
Coco <cocochen12301@gmail.com>
2025-07-23 04:18:07 -0700

    feat: Implement socket server

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,10 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 import { connectDB } from './lib/db';
-
-import http from 'http';
-import { Server } from 'socket.io';
+import { app, server } from './lib/socket';
 
 dotenv.config();
-const app = express();
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
@@ -26,36 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/gameroom", gameroomRoutes);
 app.use("/api/message", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("server is running on port: " + PORT);
   connectDB();
 });
-
-// const server = http.createServer(app);
-// server.listen(3001, () => {
-//   console.log('Server running on http://localhost:3001');
-// });
-
-// const io = new Server(server, {
-//   cors: {
-//     origin: 'http://localhost:5173',
-//     methods: ['GET', 'POST'],
-//   },
-// });
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get('/', (_req, res) => {
-//   res.send('Server is running');
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('Client connected: ', socket.id);
-
-//   socket.on('disconnect', () => {
-//     console.log('Client disconnected: ', socket.id);
-//   });
-// });
-
-
