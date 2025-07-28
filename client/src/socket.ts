7d0c7eb912d7e398e8b0d2b3fbd946265b92ecff
Coco <cocochen12301@gmail.com>
2025-07-28 02:04:10 -0700

    chore: Removed type definitions which were already shipped with socket.io client package

--- client/src/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client';
-
-export const socket = io('http://localhost:3001');
-console.log("Attempting socket connection...");
-socket.on('connect_error', (err) => {
-  console.error('Socket connection error:', err.message);
-});
